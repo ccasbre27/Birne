3fc4749fffa259e24c695853308a304a74733927
Carlos Castro Brenes <ccasbre27@hotmail.com>
2020-03-29 12:05:35 -0600

    add speech recognition in top page

--- Birne.MobileApp/Birne.MobileApp/ViewModels/HomeViewModel.cs
+++ Birne.MobileApp/Birne.MobileApp/ViewModels/HomeViewModel.cs
@@ -1,11 +1,51 @@
-﻿using Prism.Navigation;
+﻿using Birne.MobileApp.Core.Interface;
+using Prism.Commands;
+using Prism.Navigation;
+using System;
+using Xamarin.Forms;
 
 namespace Birne.MobileApp.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        private ISpeechToText _speechRecongnitionInstance;
+
+        public DelegateCommand SpeechCommand { get; private set; }
+
         public HomeViewModel(INavigationService navigationService) : base(navigationService)
         {
+            SpeechCommand = new DelegateCommand(StartSpeechRecognition);
+
+            try
+            {
+                _speechRecongnitionInstance = DependencyService.Get<ISpeechToText>();
+            }
+            catch (Exception ex)
+            {
+                //recon.Text = ex.Message;
+            }
+
+
         }
+
+        public void StartSpeechRecognition()
+        {
+            try
+            {
+                _speechRecongnitionInstance.StartSpeechToText();
+            }
+            catch (Exception ex)
+            {
+                //recon.Text = ex.Message;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                //start.IsEnabled = false;
+            }
+        }
+
+
+
     }
 }
